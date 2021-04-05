039678fb36a7d951e1799bceb7b29841bcd85975
github username <github email address>
2021-04-05 14:25:27 -0700

    before lecture 01

---
+++ 01-js-functions-and-variables/src/index.js
@@ -0,0 +1,29 @@
+// Open up the browser and test out some code in the console!
+// Lets build an app for a FlatIron Podcast
+//JavaScript Functions 
+    //Define a function
+
+    //Now with Arrow Function syntax! 
+
+    //One Line implicit returns
+
+//Defining parameters and passing arguments
+
+    
+    //Arrow functions do not need () if they are passed a single param
+
+
+//Scope 
+    //host is in global Scope 
+
+
+    //Const variables can not be overwritten 
+
+//The Call Stack
+
+
+
+//Check for understanding 
+//Draw out the call stack for the following 
+
+
