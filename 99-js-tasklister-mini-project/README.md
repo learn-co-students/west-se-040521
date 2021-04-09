bc57796cce26bbff5c6f7bc32b092928f9ae5a79
Jasper Reed <jasper.reed@flatironschool.com>
2021-04-09 12:00:36 -0700

    after lecture

--- 99-js-tasklister-mini-project/README.md
+++ 99-js-tasklister-mini-project/README.md
@@ -37,9 +37,9 @@ moving forward. If you take a look at the `index.html` file, you'll see that the
 
 ### Deliverables
 
-[] As a user, I should be able to type a task into the input field.
-[] As a user, I should be able to click some form of a submit button.
-[] As a user, I expect to see the task string that I provided appear in the DOM
+[x] As a user, I should be able to type a task into the input field.
+[x] As a user, I should be able to click some form of a submit button.
+[x] As a user, I expect to see the task string that I provided appear in the DOM
   after the submit button has been activated.
 
 **Note:** [While the example][example] shows one possible working implementation
@@ -122,15 +122,15 @@ prevent `submit` from doing its default behavior in a similar fashion.
 Once you've got the required deliverables working, you may want to try to
 implement one or more of the following:
 
-[] A delete function that will remove tasks from your list
-[] A priority value selected from a [dropdown][] that is used to determine the color
+[x] A delete function that will remove tasks from your list
+[x] A priority value selected from a [dropdown][] that is used to determine the color
   of the text in the list (e.g. red for high priority, yellow for medium, green
   for low)
   * As an additional challenge, implement a sorting functionality that displays
     the tasks in ascending or descending order based on priority
-* An additional input field (e.g. user, duration, date due)
-* Ability to edit tasks
-* Something of your choice! The main objective is to add a feature that allows
+[x] An additional input field (e.g. user, duration, date due)
+[x] Ability to edit tasks
+[] Something of your choice! The main objective is to add a feature that allows
   the user's input to affect the DOM
 
 [example]: https://learn-co-curriculum.github.io/js-task-lister-lite/
--- 99-js-tasklister-mini-project/src/index.js
+++ 99-js-tasklister-mini-project/src/index.js
@@ -1,6 +1,77 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code goes here
-  const start = () => {
-    
-  }
+  let form = document.getElementById('create-task-form')
+  form.addEventListener('submit', newTask)
 });
+
+
+const newTask = function(event) {
+  event.preventDefault()
+  let input = event.target['new-task-description'].value
+  let color = event.target['selectTask'].value
+  let user = event.target['new-task-user'].value
+  appendTask(input, color, user)
+  event.target['new-task-description'].value = ''
+  event.target['new-task-user'].value = ''
+}
+
+const appendTask = function(input, color, user) {
+  // variable declaration
+  let ul = document.getElementById('tasks')
+  let li = document.createElement('li')
+  let deleteBtn = document.createElement('button')
+  let editBtn = document.createElement('button')
+  
+  deleteBtn.innerText = 'X'
+  editBtn.innerText = 'EDIT'
+  li.innerText = `${user}: ${input} `
+  li.id = input
+  li.data = user
+  li.style.color = color
+
+  // appending
+  li.append(deleteBtn, editBtn)
+  ul.appendChild(li)
+
+  // event listeners
+  editBtn.addEventListener('click', () => editForm(ul, li))
+  deleteBtn.addEventListener('click', () => ul.removeChild(li))
+}
+
+const editForm = function(ul, li) {
+  let form = document.createElement('form')
+  let inputField = document.createElement('input')
+  let submitBtn = document.createElement('input')
+
+  inputField.type ='text'
+  inputField.name = 'editTask'
+  inputField.placeholder = li.id
+
+  submitBtn.type = 'submit'
+  submitBtn.value = 'Change'
+
+  form.append(inputField, submitBtn)
+  li.append(form)
+
+  form.addEventListener('submit', (e) => editTask(e, ul, li))
+
+}
+
+const editTask = function(e, ul, li) {
+  e.preventDefault()
+  
+  li.innerText = `${li.data}: ${e.target.editTask.value} `
+
+  let deleteBtn = document.createElement('button')
+  let editBtn = document.createElement('button')
+  
+  deleteBtn.innerText = 'X'
+  editBtn.innerText = 'EDIT'
+
+  li.append(deleteBtn, editBtn)
+
+  editBtn.addEventListener('click', () => editForm(li))
+  deleteBtn.addEventListener('click', () => ul.removeChild(li))
+
+}
+
