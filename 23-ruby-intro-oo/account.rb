74c6b46ab778783a6e5962297e5b64cfbee508b0
github username <github email address>
2021-05-18 11:53:08 -0700

    before workshop 23

---
+++ 23-ruby-intro-oo/account.rb
@@ -0,0 +1,15 @@
+require 'pry'
+# ===============
+# Procedural Ruby
+# ================
+
+
+# ===============
+# Object-Oriented Ruby
+# ===============
+
+
+
+# we could run all of the above implentation code in Pry as well (to exit Pry, just type 'exit')
+# binding.pry
+
---
+++ 23-ruby-intro-oo/demo.rb
@@ -0,0 +1,16 @@
+# ******************************
+# Everything is an Object in Ruby
+# ******************************
+
+# what is the data type of x?
+# what is the value of x?
+x = 3
+
+# ******************
+# What is an object?
+# ******************
+
+
+# *************************
+# Creating our own Objects
+# *************************
